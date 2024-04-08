3a11d59482fc6e9ee872beb7a9efb918b5f85ee5
Md Arfatul Islam Erfan <163043163+deverfan44@users.noreply.github.com>
2024-04-08 17:07:31 +0600

    Update minimum_xor.cpp

--- Week_4/day-1/minimum_xor.cpp
+++ Week_4/day-1/minimum_xor.cpp
@@ -4,26 +4,27 @@ using namespace std;
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
-
     int t;
     cin >> t;
-    while (t--) {
+    while(t--)
+    {
         int n;
         cin >> n;
         vector<int> v(n);
-        int xOr = 0;
-        for (int i=0; i<n; i++) 
+        int XOR = 0;
+        for(int i=0; i<n; i++)
         {
             cin >> v[i];
-            xOr ^= v[i];
+            XOR ^= v[i];
         }
-
-        int ans = xOr;
-        for (int i=0; i<n; i++) {
-            ans = min(ans, (xOr ^ v[i]));
+        int ans = XOR;
+        for(int i=0; i<n; i++)
+        {
+            ans = min(ans,(XOR^v[i]));
         }
-
         cout << ans << '\n';
+
     }
+
     return 0;
 }
