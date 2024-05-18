e45d1ec4ebf7ff9cbbc5dd9cb1a0c25d9c598d8c
Md Arfatul Islam Erfan <163043163+deverfan44@users.noreply.github.com>
2024-05-19 00:12:17 +0600

    Update different_divisors.cpp

--- Week_8/Day_5/different_divisors.cpp
+++ Week_8/Day_5/different_divisors.cpp
@@ -1,23 +1,44 @@
 #include <bits/stdc++.h>
 #define ll long long
 using namespace std;
+bool isPrime(int n) {
+    for(int i=2; i*i <= n; i++) {
+        if(n%i == 0) return false;
+    }
 
+    return true;
+}
+void solveIt() {
+    int d;
+    cin >> d;
+    int x = 1+d;
+    ll result1,result2;
+    while(true) {
+        if(isPrime(x)){
+            result1 = x;
+            break;
+        }
+        x++;
+    }
+    x+=d;
+    while(true) {
+        if(isPrime(x)){
+            result2 = x;
+            break;
+        }
+        x++;
+    }
+
+    ll ans = result1*result2;
+    cout << ans << '\n';
+}
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
-    vector<int> v;
-    for(int i=1; i*i<=n; i++) {
-        if(n%i==0) {
-            v.push_back(i);
-            int x=n/i;
-            if(x!=i) {
-                v.push_back(x);
-            }
-        }
-    }
-    sort(v.begin(),v.end());
-    for(int i=0; i<v.size(); i++) {
-        cout << v[i] << " ";
+    int t;
+    cin >> t;
+    while(t--) {
+        solveIt();
     }
     return 0;
 }
